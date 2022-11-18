4c2df5c4904aefbe6c7c46463e6a1547846dec8d
rensilva5 <renzoasilva@gmail.com>
2022-11-18 00:22:25 -0500

    using  event target

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
-
 const Home = () => {
+
+const handleSubmit = (e) => {
+    console.log('hello world', e)
+}
+
+const handleSubmitAgain = (name, e) => {
+    console.log('again hello ' + name, e.target)
+}
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleSubmit}>Click here</button>
+            <button onClick={(e) => handleSubmitAgain('mario', e)}>Click here</button>
         </div>
      );
 }
  
 export default Home;
-<div className="home">
-    <h2>Homepage</h2>
-</div>
