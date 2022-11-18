7f8fc2264a33774f5e332e463b736ed347796bff
rensilva5 <renzoasilva@gmail.com>
2022-11-18 10:31:29 -0500

    using useState

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [country, setCountry] = useState('france')
+    const [city, setCity] = useState('paris')
 
-const handleSubmit = (e) => {
-    console.log('hello world', e)
+const handleSubmit = () => {
+    setCountry('england')
+    setCity('london')
 }
 
-const handleSubmitAgain = (name, e) => {
-    console.log('again hello ' + name, e.target)
-}
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <h2>{ country }'s capital is {city}</h2>
             <button onClick={handleSubmit}>Click here</button>
-            <button onClick={(e) => handleSubmitAgain('mario', e)}>Click here</button>
+            {/* <button onClick={(e) => handleSubmitAgain('mario', e)}>Click here</button> */}
         </div>
      );
 }
