349e35b59c72a7dfe973bce99bb6aca51b021f6a
rensilva5 <renzoasilva@gmail.com>
2022-11-19 13:13:07 -0500

    the use of conditional template

--- src/Home.js
+++ src/Home.js
@@ -3,25 +3,27 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
-
-
+    const [isLoading, setIsLoading] = useState(true)
     // const HandleDeleteBlog = (id) => {
     //     const newBlogs = blogs.filter(blog => blog.id !== id)
     //     setBlogs(newBlogs)
     // }
-
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')  // end point
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')  // end point
         .then(res => {          //return promise
             return res.json()   //It is a syncronize, it takes sometime to do
         })
         .then(data => {        //Once above is completed, this is gonna fire a function.
-            setBlogs(data)  
+            setBlogs(data)
+            setIsLoading(false) //It takes the true statement to false when it fectchin the data.
         })
+        }, 1000)
     }, [])
 
     return ( 
         <div className="home">
+            { isLoading && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} header='another test' />}   
             </div>
      );
