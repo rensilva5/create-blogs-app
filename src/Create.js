7829f854d2556a610cc337db21b548934eb6d4da
rensilva5 <renzoasilva@gmail.com>
2022-11-23 12:40:12 -0500

    addind new contacts and disable button

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,23 @@ const Create = () => {
 const [title, setTitle] = useState('')
 const [body, setBody] = useState('')
 const [author, setAuthor] =useState('england')
+const [isPending, setIsPending] = useState(false)
+
 
 const handleSubmit = (e) => {
     e.preventDefault()
     const blog = { title, body, author }
 
-    console.log(blog)
+    setIsPending(true)
+
+    fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(blog)
+    }).then(() => {
+        console.log('contact added')
+        setIsPending(false)
+    })
 }
 
     return (
@@ -39,7 +50,8 @@ const handleSubmit = (e) => {
                     <option value='italy'>italy</option>
                     <option value='spain'>spain</option>
                 </select>
-                <button>Submit</button>
+                { !isPending && <button>Submit</button>}
+                { isPending && <button disabled>Adding a new contact</button>}
                 <p>{ title }</p>
                 <p>{ body }</p>
                 <p>{ author }</p>
